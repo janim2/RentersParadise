5196c486a12e4f35f77d4767d234a6e875fb2e95
Okai Yeboah <kingkloy16@gmail.com>
2020-12-10 02:49:34 +0000

    separate MyApp from main

---
+++ lib/src/app.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rentersparadise/src/core/bloc/cubit/walkthrough_cubit.dart';
+import 'package:rentersparadise/src/views/screens/onboard_screens/onboarding.dart';
+import 'package:rentersparadise/src/views/screens/onboard_screens/splash_screen.dart';
+
+class MyApp extends StatelessWidget {
+  static Map<int, Color> color = {
+    50: Color.fromRGBO(230, 120, 23, .1),
+    100: Color.fromRGBO(230, 120, 23, .2),
+    200: Color.fromRGBO(230, 120, 23, .3),
+    300: Color.fromRGBO(230, 120, 23, .4),
+    400: Color.fromRGBO(230, 120, 23, .5),
+    500: Color.fromRGBO(230, 120, 23, .6),
+    600: Color.fromRGBO(230, 120, 23, .7),
+    700: Color.fromRGBO(230, 120, 23, .8),
+    800: Color.fromRGBO(230, 120, 23, .9),
+    900: Color.fromRGBO(230, 120, 23, 1),
+  };
+
+  final MaterialColor colorCustom = MaterialColor(0xffe67f17, color);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Renters Paradise',
+      theme: ThemeData(
+        primarySwatch: colorCustom,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: BlocBuilder<WalkthroughCubit, WalkthroughState>(
+        builder: (context, state) {
+          if (state is WalkthroughInitial) {
+            return MySplashScreenPage();
+          } else if (state is WalkthroughFinished) {
+            return Onboarding();
+          } else {
+            return MySplashScreenPage();
+          }
+        },
+      ),
+    );
+  }
+}
