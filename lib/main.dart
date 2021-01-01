b21e5f78a893e9c7566d86f713a4099288a933b4
lateofrederick <lateofrederick@gmail.com>
2021-01-01 12:44:21 +0000

    initialised the PushNotification class

--- lib/main.dart
+++ lib/main.dart
@@ -6,18 +6,18 @@ import 'package:rentersparadise/src/core/bloc/cubit/favourite_properties_cubit.d
 import 'package:rentersparadise/src/core/bloc/cubit/notification_cubit.dart';
 import 'package:rentersparadise/src/core/bloc/cubit/select_property_images_cubit.dart';
 import 'package:rentersparadise/src/core/bloc/cubit/walkthrough_cubit.dart';
+import 'package:rentersparadise/src/core/services/push_notification.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await PushNotification.init();
   runApp(
     MultiBlocProvider(providers: [
       BlocProvider(
         create: (context) => WalkthroughCubit(),
       ),
-      BlocProvider(
-        create: (context) => NotificationCubit()
-        ),
+      BlocProvider(create: (context) => NotificationCubit()),
       BlocProvider(create: (context) => FavouritePropertiesCubit()),
       BlocProvider(create: (context) => SelectPropertyImagesCubit())
     ], child: MyApp()),
